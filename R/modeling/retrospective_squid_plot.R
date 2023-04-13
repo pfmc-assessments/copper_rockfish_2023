2ebd18a871b9d58fd073d12f75d99700ffb63a0e
Chantel Wetzel <chantelwetzel@noaa.gov>
2023-04-13 08:13:58 -0700

    add table of values

--- R/modeling/retrospective_squid_plot.R
+++ R/modeling/retrospective_squid_plot.R
@@ -49,3 +49,69 @@ SSplotRetroRecruits(retroSummary = mysummary,
                     mcmcVec=FALSE,devs=TRUE,
                     relative=FALSE,labelyears=TRUE,legend=FALSE,leg.ncols=4)
 dev.off()
+
+#=====================================================================================================
+# Create a table of parameter values to understand what is changing across retrospective runs
+#=====================================================================================================
+
+x <- mysummary
+ii <- 1:length(modelnames)
+n  <- length(modelnames)
+out <- matrix(NA, 24, max(ii))
+
+out <- rbind(
+  as.numeric(x$likelihoods[x$likelihoods$Label == "TOTAL",1:n]), 
+  as.numeric(x$likelihoods[x$likelihoods$Label == "Survey",1:n]), 
+  as.numeric(x$likelihoods[x$likelihoods$Label == "Length_comp",1:n]),
+  as.numeric(x$likelihoods[x$likelihoods$Label == "Age_comp",1:n]), 
+  as.numeric(x$likelihoods[x$likelihoods$Label == "Recruitment",1:n]), 
+  as.numeric(x$likelihoods[x$likelihoods$Label == "Forecast_Recruitment",1:n]),
+  as.numeric(x$likelihoods[x$likelihoods$Label == "Parm_priors",1:n]),
+  as.numeric(x$pars[x$pars$Label == "SR_LN(R0)", 1:n]), 
+  as.numeric(x$SpawnBio[x$SpawnBio$Label == "SSB_Virgin", 1:n]),
+  as.numeric(x$SpawnBio[x$SpawnBio$Label == "SSB_2023", 1:n]),
+  as.numeric(x$Bratio[x$Bratio$Label == "Bratio_2023", 1:n]), 
+  as.numeric(x$quants[x$quants$Label == "Dead_Catch_SPR", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "SR_BH_steep", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "NatM_uniform_Fem_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "L_at_Amin_Fem_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "L_at_Amax_Fem_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "VonBert_K_Fem_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "CV_young_Fem_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "CV_old_Fem_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "NatM_uniform_Mal_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "L_at_Amin_Mal_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "L_at_Amax_Mal_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "VonBert_K_Mal_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "CV_young_Mal_GP_1", 1:n]),
+  as.numeric(x$pars[x$pars$Label == "CV_old_Mal_GP_1", 1:n]) )  
+
+out = as.data.frame(out)
+colnames(out) = modelnames
+rownames(out) = c("Total Likelihood",
+                  "Survey Likelihood",
+                  "Length Likelihood",
+                  "Age Likelihood",
+                  "Recruitment Likelihood",
+                  "Forecast Recruitment Likelihood",
+                  "Parameter Priors Likelihood",
+                  "log(R0)",
+                  "SB Virgin",
+                  "SB 2023",
+                  "Fraction Unfished 2023",
+                  "Total Yield - SPR 50",
+                  "Steepness",
+                  "Natural Mortality - Female",
+                  "Length at Amin - Female",
+                  "Length at Amax - Female",
+                  "Von Bert. k - Female",
+                  "CV young - Female",
+                  "CV old - Female",
+                  "Natural Mortality - Male",
+                  "Length at Amin - Male",
+                  "Length at Amax - Male",
+                  "Von Bert. k - Male",
+                  "CV young - Male",
+                  "CV old - Male")
+
+write.csv(out, file = file.path(retro.folder, paste0(base_name, "_retros.csv")))
