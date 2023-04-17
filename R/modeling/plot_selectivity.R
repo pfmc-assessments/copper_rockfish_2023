3f2c485efd4ce7ec1a3cbd5a3b549b919367ff4a
Chantel Wetzel <chantelwetzel@noaa.gov>
2023-04-17 16:49:32 -0700

    add north model runs

--- R/modeling/plot_selectivity.R
+++ R/modeling/plot_selectivity.R
@@ -1,6 +1,7 @@
 
 library(r4ss)
 area <- "sca"
+area <- 'nca'
 
 user <- Sys.getenv("USERNAME")
 if( grepl("Chantel", user) ){
@@ -18,12 +19,14 @@ fleets <- model$FleetNames
 info1 <- SSplotSelex(model, fleets = 1, fleetnames = fleets[1], subplot = 1, year = c(2001, 2016, 2017))
 #info1$infotable$col <- rich.colors.short(8)[c(1,1)]
 
-info2 <- SSplotSelex(model, fleets = 2, fleetnames = fleets[2], subplot = 1, year = c(2021, 2022))
+info2 <- SSplotSelex(model, fleets = 2, fleetnames = fleets[2], subplot = 1, year = c(1916, 2022))
 #info1$infotable$col <- rich.colors.short(8)[c(1,1)]
 
-info3 <- SSplotSelex(model, fleets = 3, fleetnames = fleets[3], subplot = 1, year = c(1999, 2021, 2022))
+info3 <- SSplotSelex(model, fleets = 3, fleetnames = fleets[3], subplot = 1, year = c(1916, 2002, 2017, 2022))
 info1$infotable$col <- rich.colors.short(8)[c(1,1)]
 
+info4 <- SSplotSelex(model, fleets = 4, fleetnames = fleets[4], subplot = 1, year = c(1916, 2002, 2017, 2022))
+info1$infotable$col <- rich.colors.short(8)[c(1,1)]
 
-info4 <- SSplotSelex(model, fleets = 4, fleetnames = fleets[4], subplot = 1, year = c(1999, 2021, 2022))
+info5 <- SSplotSelex(model, fleets = 5, fleetnames = fleets[5], subplot = 1, year = c(2016, 2022))
 info1$infotable$col <- rich.colors.short(8)[c(1,1)]
