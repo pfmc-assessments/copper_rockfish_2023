51953b04ac185b00a2123bf3db66f72569bec7b3
Chantel Wetzel <chantelwetzel@noaa.gov>
2023-05-26 12:50:52 -0700

    bivariate running and plotting

--- R/modeling/bivariate_Mh_profile.R
+++ R/modeling/bivariate_Mh_profile.R
@@ -26,10 +26,12 @@ getLtable=function(modelsum, legend.labels=NULL) {
 }
 
 #setwd("~/Rockfish/Rockfish assessment/vermillion 2021/SSmodel")
-setwd("C:/Assessments/2023/copper_rockfish_2023/models/nca/_sensitivities/bivariat_m_h")
+setwd("C:/Assessments/2023/copper_rockfish_2023/models/nca/_sensitivities/9.8_selex_fix_bivariate_m_equal")
+setwd("C:/Assessments/2023/copper_rockfish_2023/models/sca/_sensitivities/14.0_base_bivariate_m_both")
+
 
 # set 1 #####
-mydir = file.path(getwd(), "9.8_selex_fix")
+mydir = getwd()
 starter <- SS_readstarter(file.path(mydir, "starter.ss"))
 # change control file name in the starter file
 starter[["ctlfile"]] <- "control_modified.ss"
@@ -47,7 +49,8 @@ profile <- r4ss::profile(
   dir = mydir, # directory
   oldctlfile = "control.ss_new",
   newctlfile = "control_modified.ss",
-  linenum = c(73, 122), #mortality, steepness
+  #linenum = c(73, 122), #mortality, steepness
+  linenum = c(76, 125), # sca
   profilevec = par_table,
   extras = "-nohess -nox"
 )
@@ -87,13 +90,15 @@ rownames(sumtablelong) = NULL
 Sdgrid = as.data.frame(sumtablelong)
 
 #john's results
-Sdgrid1=readxl::read_xlsx("bivariate_post-STAR/southbase.bivariate.table.xlsx",sheet = "longformat")
-Sdgrid=rbind(Sdgrid1,Sdgrid)
-Sdgrid=dplyr::arrange(Sdgrid,h,M)
-write.csv(Sdgrid,"bivariate_post-STAR/south_bivariate_table.csv",row.names = F)
+#Sdgrid1=readxl::read_xlsx("bivariate_post-STAR/southbase.bivariate.table.xlsx",sheet = "longformat")
+#Sdgrid=rbind(Sdgrid1,Sdgrid)
+#Sdgrid=dplyr::arrange(Sdgrid,h,M)
+#write.csv(Sdgrid,"bivariate_post-STAR/south_bivariate_table.csv",row.names = F)
 
 #base model
-basemodel <- SS_output("C:/Assessments/2023/copper_rockfish_2023/models/nca/_sensitivities/9.8_selex_fix")
+basemodel <- SS_output("C:/Assessments/2023/copper_rockfish_2023/models/nca/9.8_selex_fix")
+basemodel <- SS_output("C:/Assessments/2023/copper_rockfish_2023/models/sca/14.0_base_forecast")
+
 Mbase=basemodel$parameters["NatM_uniform_Fem_GP_1","Value"]
 hbase=basemodel$parameters["SR_BH_steep","Value"]
 nllbase=basemodel$likelihoods_used["TOTAL","values"]
@@ -103,29 +108,34 @@ nllbase=basemodel$likelihoods_used["TOTAL","values"]
 #model with min NLL
 Sdgridmin=Sdgrid[which.min(Sdgrid$NLL),]
 #models within 2 of min NLL
-Sdgridminset=Sdgrid[which(abs(Sdgrid$NLL-nllbase) < 1.96),]
+Sdgridminset=Sdgrid[which(abs(Sdgrid$NLL-nllbase) < 2.99),]
+Sdgridnll=Sdgrid[Sdgrid$NLL == min(Sdgrid$NLL),]
 
-p1=ggplot(Sdgrid,aes(x=M,y=h,fill=NLL)) +
+
+ggplot(Sdgrid,aes(x=M,y=h,fill=NLL)) +
   geom_tile() +
   geom_tile(data=Sdgridminset, color="tomato", lwd=1, fill=NA) +
-  geom_point(data=data.frame(M=Mbase,h=hbase), fill="white", color="black",pch=21, size=2) +
-  geom_text(aes(label=round(NLL,1)), color="white", size = 2.5, fontface = "bold") +
+  #geom_tile(data=Sdgridnll, color="white", lwd=1, fill=NA) +
+  geom_point(data=data.frame(M=Mbase,h=hbase), fill="white", color="black",pch=21, size=4) +
+  geom_text(aes(label=round(NLL,1)), color="white", size = 4, fontface = "bold") +
   #geom_text(data=Sdgridmin, aes(label=round(NLL,1)), color="tomato") +
-  theme_bw() + scale_fill_viridis_c(direction = -1) +
+  theme_bw() + scale_fill_viridis_c(direction = 1) +
   scale_x_continuous(expand = c(0,0), breaks = unique(Sdgrid$M)) + 
-  scale_y_continuous(expand = c(0,0), breaks = unique(Sdgrid$h))
-ggsave("bivariate_post-STAR/S_Mhgrid_nll.png",width = 5.5,height = 4)
+  scale_y_continuous(expand = c(0,0), breaks = unique(Sdgrid$h)) +
+  theme_bw(base_size = 18)
+ggsave("_plots/S_Mhgrid_nll_both_sex_m_2.99.png",width = 10, height = 10)
 
-p2=ggplot(Sdgrid,aes(x=M,y=h,fill=Depletion)) +
+ggplot(Sdgrid,aes(x=M,y=h,fill=Depletion)) +
   geom_tile() +
   geom_tile(data=Sdgridminset, color="tomato", lwd=1, fill=NA) +
-  geom_point(data=data.frame(M=Mbase,h=hbase), fill="white", color="black",pch=21, size=2) +
-  geom_text(aes(label=round(Depletion,2)), color="white", size = 5, parse = TRUE) +
+  geom_point(data=data.frame(M=Mbase,h=hbase), fill="white", color="black",pch=21, size=4) +
+  geom_text(aes(label=round(Depletion,2)), color="white", size = 5, parse = TRUE, fontface = "bold") +
   #geom_text(data=Sdgridmin, aes(label=round(Depletion,2)), color="tomato") +
   theme_bw() + scale_fill_viridis_c(direction = 1) +
   scale_x_continuous(expand = c(0,0), breaks = unique(Sdgrid$M)) + 
-  scale_y_continuous(expand = c(0,0), breaks = unique(Sdgrid$h))
-#ggsave("bivariate_post-STAR/S_Mhgrid_depletion.png",width = 5.5,height = 4)
+  scale_y_continuous(expand = c(0,0), breaks = unique(Sdgrid$h)) +
+  theme_bw(base_size = 18)
+ggsave("_plots/S_Mhgrid_depletion_both_sex_m_2.99.png",width = 10, height = 10)
 
 #p3=ggplot(Sdgrid,aes(x=M,y=h,fill=EquilMSY)) +
 #  geom_tile() +
@@ -166,6 +176,8 @@ out$diffNegLogLike <- out$negLogLike - min(out$negLogLike)#out.mle["negLogLike"]
 #out$diff_M <- out$M_f - out$M_m
 out <- out[order(out$M, out$h),]
 
+find <- out[out$negLogLike == min(out$negLogLike), ]
+
 x <- unique(round(out$M, 4))
 y <- unique(round(out$h, 4))
 z <- matrix(out$diffNegLogLike, 
@@ -173,9 +185,10 @@ z <- matrix(out$diffNegLogLike,
             byrow = TRUE, 
             dimnames = list(as.character(x), as.character(y)))
 
-library(reshape2); library(met$)
+library(reshape2); library(metR)
 mtrx_melt <- melt(z, id.vars = c("M", "h"), measure.vars = 'Delta_NLL')
 names(mtrx_melt) = c("M", "h", "Delta_NLL")
+min_nll <- mtrx_melt[mtrx_melt$Delta_NLL, ]
 
 # Plot_ly figure
 #plot_ly(mtrx_melt, x = ~M_f, y = ~M_m, z = ~Delta_NLL, type = 'contour', 
@@ -185,9 +198,11 @@ names(mtrx_melt) = c("M", "h", "Delta_NLL")
 ggplot(mtrx_melt, aes(x = M, y = h)) +
     geom_contour_filled(aes(z = Delta_NLL), breaks = c(0, 2, 3, 4, 6, 10, 20, 50, seq(100, 600, 100))) +
     geom_point(aes (x = 0.108, y = 0.72),  size = 4, col = "white") +
+    geom_point(aes (x = find[,1], y = find[,2]),  size = 4, col = "white") +
+    annotate("text", x = find[,1] + 0.015, y = find[,2], label = "Lowest NLL", size =10, col = 'white') +
     annotate("text", x = 0.12, y = 0.72, label = "Base Model", size =10, col = 'white') +
     geom_text_contour(aes(z = Delta_NLL), 
-       breaks = c(2, 4, 6, seq(10, 30, 10)), size = 7, color = 'white') +
+       breaks = c(3, 5, 7, seq(10, 30, 10)), size = 7, color = 'white') +
     xlab("Natural Mortality (F)") +
     ylab("Steepness (h)") +
     scale_x_continuous(breaks = seq(0.05, 0.15, 0.01)) +
@@ -199,6 +214,7 @@ ggplot(mtrx_melt, aes(x = M, y = h)) +
       axis.title.y = element_text(size = 20),
       legend.text = element_text(size = 15), 
       legend.title = element_text(size = 15)) +
-    guides(fill = guide_legend(title = "Change in NLL"))
-ggsave(file.path(getwd(), "joint_m_h_profile_ggsave.png"), width = 14, height = 12)
+    guides(fill = guide_legend(title = "Change in NLL")) +
+    theme_bw(base_size = 16)
+ggsave(file.path(getwd(), "_plots", "joint_m_h_profile_ggsave.png"), width = 14, height = 12)
 #dev.off()
