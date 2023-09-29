46a86e3213ea17f08010db86d7aa7ee2cf148473
Chantel Wetzel <chantelwetzel@noaa.gov>
2023-09-29 10:22:10 -0700

    add projection split table

--- documents/helper_functions.R
+++ documents/helper_functions.R
@@ -206,6 +206,84 @@ create_projection_table <- function(
   
 }
 
+
+create_projection_by_area_table <- function(
+    management_dir = management_dir, 
+    doc_dir = doc_dir, 
+    model_dir = model_dir,
+    fixed_removals = c(91.53, 94.69),
+    doc_names = c("nca", 'sca'),
+    years = 2023:2034,
+    north_4010_percent = 0.0586,
+    table_names = c("copper_ca_north.csv", "copper_ca_south.csv"),
+    prefix = NULL){
+  
+  if(!is.null(prefix)) { prefix <- paste0(prefix, "-")}
+  
+  load(file.path(doc_dir, doc_names[1], "00mod.Rdata"))
+  model1 <- model
+  load(file.path(doc_dir, doc_names[2], "00mod.Rdata"))
+  model2 <- model
+  
+  sb1 <- model1$derived_quants[model1$derived_quants$Label %in% paste0("SSB_", years), "Value"]
+  sb2 <- model2$derived_quants[model2$derived_quants$Label %in% paste0("SSB_", years), "Value"]
+  sb0 <- model1$derived_quants[model1$derived_quants$Label == "SSB_Virgin", "Value"] +
+    model2$derived_quants[model2$derived_quants$Label == "SSB_Virgin", "Value"]
+  
+  sb <- sb1 + sb2
+  depl <- sb / sb0
+  
+  # read in the management tables for north and south of 4010 in CA
+  table1 <- read.csv(file.path(management_dir, table_names[1]))
+  table2 <- read.csv(file.path(management_dir, table_names[2]))
+  
+  ofl <- table1$OFL[table1$Year %in% 2023:2024] + table2$OFL[table2$Year %in% 2023:2024]
+  acl <- table1$ACL[table1$Year %in% 2023:2024] + table2$ACL[table2$Year %in% 2023:2024]
+
+  table_proj <- read.csv(file.path(model_dir, "Projection_Values.csv"))
+  
+  est_ofl <- table_proj$OFL
+  est_abc <- table_proj$ABC
+  est_acl <- table_proj$ACL
+  
+  buffer <- est_abc/est_ofl
+  south_acl_4010 <- table_proj$Removals.Model1 + table_proj$Removals.Model2 - north_4010_percent * table_proj$Removals.Model2
+  north_acl_4010 <- north_4010_percent * table_proj$Removals.Model2
+  
+  south_ofl_4010 <- table_proj$OFL1 + table_proj$OFL2 - north_4010_percent * table_proj$OFL2
+  north_ofl_4010 <- north_4010_percent * table_proj$OFL2
+  
+  south_abc_4010 <- table_proj$ABC1 + table_proj$ABC2 - north_4010_percent * table_proj$ABC2
+  north_abc_4010 <- north_4010_percent * table_proj$ABC2
+  
+  out <- data.frame(
+    Year = years, 
+    removals = c(fixed_removals, rep("-", 10)),
+    south_ofl = c("-", "-", round(south_ofl_4010, 2)),
+    south_abc = c("-", "-", round(south_abc_4010, 2)),
+    south_acl = c("-", "-", round(south_acl_4010, 2)),
+    north_ofl = c("-", "-", round(north_ofl_4010, 2)),
+    north_abc = c("-", "-", round(north_abc_4010, 2)),
+    north_acl = c("-", "-", round(north_acl_4010, 2))
+  )
+  
+  col_names <- c("Year", 
+                 "Assumed Catch (mt)",
+                 "South OFL (mt)", "South ABC (mt)", "South ACL (mt)", 
+                 "North OFL (mt)", "North ABC (mt)", "North ACL (mt)")
+  
+  sa4ss::table_format(
+    x = out,
+    caption = "The estimated California OFL (mt), ABC (mt), and ACL (mt) split south and north $40^\\circ 10^\\prime$ N. lat. The estimated percent allocated to the north $40^\\circ 10^\\prime$ N. lat. in California is 5.86 percent based on estimates of rocky habitat and density of copper rockfish in the area.",
+    label = paste0(prefix, "ca-proj-split"),
+    col_names = col_names,
+    custom_width = TRUE,
+    col_to_adjust = 2:8, 
+    width = rep("1.3 cm", 7)
+  )  
+  
+}
+
 plot_combined <- function(
   doc_dir = doc_dir, 
   doc_names = c("nca", 'sca'),
